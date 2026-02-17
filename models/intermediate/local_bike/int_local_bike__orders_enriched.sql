e62e33695c005ef96af864e4b086d5a9879e0f7c
Holy Rakotomalala <holyrakotoshop64@gmail.com>
2026-02-17 13:54:48 +0000

    modifying-changes

--- models/intermediate/local_bike/int_local_bike__orders_enriched.sql
+++ models/intermediate/local_bike/int_local_bike__orders_enriched.sql
@@ -9,16 +9,12 @@ with orders_info as (
         required_date,
         shipped_date,
         store_id,
-        staff_id
-        
-    from  {{ref('stg_local_bike__orders')}}
-),
-
-orders_details as (
-
-select 
-
-        order_id, 
+        store_name,
+        city,
+        state,
+        staff_id,
+        full_name,
+        active, 
         count (distinct item_id) as nb_distinct_items, 
         count (distinct product_id) as nb_distinct_products, 
         sum(quantity) as quantity,
@@ -27,65 +23,30 @@ select
         sum(discounted_amount) as discounted_amount
 
 from {{ref('int_local_bike__order_items_enriched')}}
-group by order_id 
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13
 
-),
-
-store_info as (
-
-    select 
-
-        store_id,
-        store_name,
-        city,
-        state
-    
-    from {{ref('stg_local_bike__stores')}}
-), 
-
-
-staff_info as (
-
-    select
-
-        staff_id,
-        CONCAT(first_name, ' ', last_name) AS full_name,
-        active,
-        store_id
-
-    from {{ref('stg_local_bike__staffs')}}
 )
 
 select 
 
-    orders_info.order_id,
-    orders_info.customer_id,
-    orders_info.order_status,
-    orders_info.order_date,
-    orders_info.required_date,
-    orders_info.shipped_date,
-    orders_info.store_id,
-    store_info.store_name,
-    store_info.city,
-    store_info.state,
-    orders_info.staff_id,
-    staff_info.full_name,
-    staff_info.active,
-    orders_details.nb_distinct_items,
-    orders_details.nb_distinct_products,
-    orders_details.quantity,
-    round(orders_details.total_amount,1) as total_amount,
-    round(orders_details.total_amount_discounted,1) as total_amount_discounted,
-    round(orders_details.discounted_amount,1) as discounted_amount
-
+    order_id,
+    customer_id,
+    order_status,
+    order_date,
+    required_date,
+    shipped_date,
+    store_id,
+    store_name,
+    city,
+    state,
+    staff_id,
+    full_name,
+    active,
+    nb_distinct_items,
+    nb_distinct_products,
+    quantity,
+    round(total_amount,1) as total_amount,
+    round(total_amount_discounted,1) as total_amount_discounted,
+    round(discounted_amount,1) as discounted_amount
 
  from orders_info
- inner join orders_details 
-    on orders_info.order_id = orders_details.order_id
- left join store_info
-    on orders_info.store_id = store_info.store_id 
-left join staff_info 
-    on orders_info.staff_id = staff_info.staff_id
-    and orders_info.store_id = staff_info.store_id
-    
-
---
+++ models/mart/local_bike/mrt_local_bike__stock_report.sql
@@ -0,0 +1,37 @@
+with product_details as (
+
+    select
+
+        product_id,
+        category_id,
+        category_name,
+        brand_id,
+        brand_name, 
+        model_year,
+        sum(quantity) as quantity_sold, 
+        store_id,
+        stock,
+        round(sum(amount_without_discount),1) as amount_sold_without_discount,
+        round(sum(amount_with_discount),1) as amount_sold_with_discount,
+        round(sum(discounted_amount),1) as discounted_amount
+
+    from {{ref('int_local_bike__order_items_enriched')}}
+    group by 1,2,3,4,5,6
+)
+
+select 
+
+  product_id,
+  store_id,
+  stock,
+  category_id,
+  category_name, 
+  brand_id,
+  brand_name,
+  model_year,
+  quantity_sold,
+  amount_sold_without_discount,
+  amount_sold_with_discount,
+  discounted_amount
+
+from product_details
